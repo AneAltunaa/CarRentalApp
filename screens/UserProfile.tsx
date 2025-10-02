6114b69850df02f324dfba056359f37576e68870
AneAltunaa <ane.altuna@opendeusto.es>
2025-10-02 10:57:22 +0200

    something profile

--- screens/UserProfile.tsx
+++ screens/UserProfile.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Button } from 'react-native/types_generated/index';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {getUser} from '../data/user';
+
 
 const UserProfile = () => {
+
+  const getUserInfo = async() => {
+      const storedUser = await AsyncStorage.getItem('user');
+      const user= storedUser ? JSON.parse(storedUser) : null;
+
+      const userInfo = getUser(user.email);
+      return userInfo;
+  }
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
   return (
     // A container view to center the content
     <View style={styles.container}>
-      <Text></Text>
+      <Button title="Back"/>
+
+
+      <Text>Email: {}</Text>
+      <Text>Name: </Text>
+      <Text>Surname: </Text>
+      <Text>Date of Birth: </Text>
+      <Text>CPR: </Text>
     </View>
   );
 };
